7d5b37052d196a815993e9d3c0465d2a247e5689
Gogo602 <gogomarosi61@gmail.com>
2025-08-03 22:45:39 +0300

    File:Indexing-Slicing.py; learnt indexing and how to slice ietms

---
+++ Indexing-Slicing.py
@@ -0,0 +1,25 @@
+
+# Indexing and slicing
+# Indexing has to do with how we access an item using its index
+
+lst = ["one", "two", "three", "four", "Five"]
+
+# get item as index 0, 
+print(lst[0])
+
+# get the last item
+print(lst[-1])
+
+
+# slicing
+# get the item from index 0 to 3
+print(lst[0:3])
+
+# get item from 1 to the end
+print(lst[1::]) 
+
+# get item at index -2 to the end
+print(lst[-2::])
+
+
+
