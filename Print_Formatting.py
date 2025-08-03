0b7cda841e02eebcc36cd3b9bc1dd9c35d2a2e7c
Gogo602 <gogomarosi61@gmail.com>
2025-08-03 23:46:08 +0300

    File: Print_Formatting.py; different ways of formatting Strings in Python

---
+++ Print_Formatting.py
@@ -0,0 +1,13 @@
+# Different ways of formatting Strings in Python
+
+# using format
+name = "Gogo"
+print("Hello {}, welcome to Python 101".format(name))
+
+# using f, which is pretty the modern way
+print(f"Hello {name}, welcome to Python 101")
+
+# older way, using %s
+print("Hello %s, welcome to Python 101" % name)
+
+# all the above way for formatting strings works but modern apprach is recommended
