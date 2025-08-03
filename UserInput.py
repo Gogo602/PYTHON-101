cf2aed398052334802fcc7db7cadc29c425d9605
Gogo602 <gogomarosi61@gmail.com>
2025-08-03 22:58:25 +0300

    File: UserInput.py; leanrt how to accept inputs from the user

---
+++ UserInput.py
@@ -0,0 +1,16 @@
+# how to accept input from users and use it in python programs
+# Note inputs always comes back as a string
+
+
+# get the users age
+age = input("What is your age: ")
+
+# multiply the age by 7
+dog_years = age * 7
+# prints it, which will in turn prints the user input 7 times since its a string
+print(dog_years)
+
+
+# 2
+name = input("What is your name: ")
+print(f"Hello {name}, hope youre good today")
