eeb8642bfbb04388a98f6124200cc2f7c490cd60
Gogo602 <gogomarosi61@gmail.com>
2025-08-04 17:36:22 +0300

    File: Multiple-Comparison; AND|OR

---
+++ Mutiple-Comparison.py
@@ -0,0 +1,18 @@
+# Multiple comparison in Python. <and> <or>
+
+
+# using AND, both conditions must be True inorder to get a True
+age = 11
+name = "Kalob"
+
+if age >= 18 and name == "Kalob":
+    print("I have decisons of my own")
+else:
+    print("You dont have right")
+
+
+# using OR, one of the conditions must be True to get a True
+if age >= 18 or name == "Kalob":
+    print("I have decisons of my own")
+else:
+    print("You dont have right")
